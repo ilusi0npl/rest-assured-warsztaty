7104f8dfb8da2bcde159a6f5be816a7509999eb4
ilusi0npl <mateuszciolek@gmail.com>
2019-08-04 12:49:08 +0200

    New packages structure and classes for chapter2

---
+++ src/test/io/swagger/petstore/test/chapter2/serialization/list/ListExample.java
@@ -0,0 +1,26 @@
+package io.swagger.petstore.test.chapter2.serialization.list;
+
+import io.swagger.petstore.test.chapter2.serialization.single.object.pojo.Pet;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class ListExample {
+
+    @Test
+    public void arrayExample() {
+        List<Pet> arrayOfPets = given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .accept("application/json")
+                .queryParam("status", "sold")
+                .get("https://petstore.swagger.io/v2/pet/findByStatus")
+                .then().assertThat().statusCode(200).extract().response().body().jsonPath().getList(".", Pet.class);
+
+        arrayOfPets.forEach(System.out::println);
+    }
+
+}
