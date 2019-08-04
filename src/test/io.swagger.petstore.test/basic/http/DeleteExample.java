73d4b0f4d96c106e1f6e9d2f4d7d2898c7801ecb
ilusi0npl <mateuszciolek@gmail.com>
2019-08-04 11:19:00 +0200

    Added tests for basic HTTP methods

---
+++ src/test/io.swagger.petstore.test/basic/http/DeleteExample.java
@@ -0,0 +1,25 @@
+package io.swagger.petstore.test.basic.http;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DeleteExample {
+
+    @Test
+    public void deleteTest() {
+        Response response = given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .accept("application/json")
+                .pathParam("petId", "8315298304486418000")
+                .delete("https://petstore.swagger.io/v2/pet/{petId}")
+                .then().assertThat().statusCode(200).extract().response();
+        System.out.println("RESPONSE WAS");
+        System.out.println(response.prettyPrint());
+    }
+
+
+}
---
+++ src/test/io.swagger.petstore.test/basic/http/PutExample.java
@@ -0,0 +1,42 @@
+package io.swagger.petstore.test.basic.http;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PutExample {
+
+    @Test
+    public void postTest() {
+        Response response = given()
+                .log()
+                .all()
+                .contentType("application/json")
+                .accept("application/json")
+                .body("{\n" +
+                        "  \"id\": 8315298304486418000,\n" +
+                        "  \"category\": {\n" +
+                        "    \"id\": 0,\n" +
+                        "    \"name\": \"string\"\n" +
+                        "  },\n" +
+                        "  \"name\": \"Burek\",\n" +
+                        "  \"photoUrls\": [\n" +
+                        "    \"http://dogs.photo.pl/burek1.img\"\n" +
+                        "  ],\n" +
+                        "  \"tags\": [\n" +
+                        "    {\n" +
+                        "      \"id\": 0,\n" +
+                        "      \"name\": \"string\"\n" +
+                        "    }\n" +
+                        "  ],\n" +
+                        "  \"status\": \"available\"\n" +
+                        "}")
+                .post("https://petstore.swagger.io/v2/pet")
+                .then().assertThat().statusCode(200).extract().response();
+        System.out.println("RESPONSE WAS");
+        System.out.println(response.prettyPrint());
+    }
+
+
+}
